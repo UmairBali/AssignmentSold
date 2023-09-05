c80531bcde7de4919de223ef11c9620eedd503f8
Umair <umairxmerri1@gmail.com>
2023-09-05 02:01:24 -0700

    5 september code

--- src/tips.jsx
+++ src/tips.jsx
@@ -2,3 +2,34 @@
 // Always Make a separate css file for a separate component
 // Alwasy call you component in App.js for a specific time
 // Use rfc for creation of a react functional component
+
+// import { useState } from "react";
+
+
+
+// Tips for the variables
+
+// There are different types of variables, most frequently used are, numeric, string and arrays and boolean repectively.
+
+
+// Below is an example of array variable
+
+
+// const[data, setData] = useState([]);
+
+// Use of array varible:
+// We use the array varibale when there are more than one records to be stored in a variable.
+
+
+// String variable
+// const [umair , setUmair] = useState("");
+// We use the array varibale when there is only one record to be stored in a variable.
+
+// Numeric variable:
+//  const[salman, setSalman] = useState(0);
+// These are used to store only one number
+// these are used to store only numerical values in them.
+
+// Boolean variable:
+// const [boolean, setBoolean] = useState(false);
+// These are used as a flag, they can be evaluated, either to false or to true.
