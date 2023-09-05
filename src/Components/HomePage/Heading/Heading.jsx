4aafc010f4b6a0f74da64a45614127d035b52b9f
Umair <umairxmerri1@gmail.com>
2023-09-05 05:57:17 -0700

    Conditional Rendering Work

--- src/Components/HomePage/Heading/Heading.jsx
+++ src/Components/HomePage/Heading/Heading.jsx
@@ -1,16 +1,26 @@
 import React from "react";
 import "./Heading.css";
 import { useState } from "react";
+import KeyboardDoubleArrowLeftIcon from '@mui/icons-material/KeyboardDoubleArrowLeft';
+import KeyboardDoubleArrowRightIcon from '@mui/icons-material/KeyboardDoubleArrowRight';
 
 export default function Heading() {
   const [num, setNum] = useState(0);
   const [count, setCount] = useState(1);
   const [name, setName] = useState("Click to Change Name");
-
+const [score, setscore] = useState(0)
   const handleIncrementClick = () => {
     setNum(num + 1);
   };
-
+ const handlebackwardclick = () => {
+  setscore(score + 1);
+ };
+ const handleforwaedclick = () => {
+  setscore(score - 1);
+ };
+  const handleresetclick = () => {
+    setscore(0)
+  }
   const handleDecClick = () => {
     setNum(num - 1);
   };
@@ -43,25 +53,51 @@ export default function Heading() {
       <div className="pb-3 jkk justifycentre aligncentre container">
         LIVE OFF MARKET INVENTORY
       </div>
-      <div className="container jaadu">Hello {num}</div>
-      <div className="container">
-        <div className="row pb-5">
-          <div className="col-lg-1">
-            <button onClick={handleIncrementClick} className="btn btn-primary">
-              Increment
-            </button>
-          </div>
-          <div className="col-lg-1">
-            <button onClick={handleDecClick} className="btn btn-warning">
-              Decrement
-            </button>
-          </div>
-          <div className="col-lg-1">
-            <button onClick={handleResClick} className="btn btn-danger">
-              Reset
-            </button>
+      {/* Salman Work */}
+      <div className="salmanwork">
+        <div className="container jaadu">Hello {num}</div>
+        <div className="container">
+          <div className="row pb-5">
+            <div className="col-lg-1">
+              <button
+                onClick={handleIncrementClick}
+                className="btn btn-primary"
+              >
+                Increment
+              </button>
+            </div>
+            <div className="col-lg-1">
+              <button onClick={handleDecClick} className="btn btn-warning">
+                Decrement
+              </button>
+            </div>
+            <div className="col-lg-1">
+              <button onClick={handleResClick} className="btn btn-danger">
+                Reset
+              </button>
+            </div>
           </div>
         </div>
+      </div>
+      {/* Umair Work */}
+      <div className="umairWork">
+    <div className="container jaadu">Total Score:  {score}
+    <div className="row">
+      <div className="col-lg-2">
+<button onClick={handleforwaedclick} className="c-btn" type="button"><KeyboardDoubleArrowLeftIcon/></button>
+      </div>
+      <div className="col-lg-2">
+<button onClick={handlebackwardclick} className="c-btn" type="button"><KeyboardDoubleArrowRightIcon/></button>
+      </div>
+      <div className="col-lg-2">
+<button onClick={handleresetclick} className="c-btn" type="button">Reset</button>
+      </div>
+    </div>
+    </div>
+{score === 10 ? (    <div className="container jaadu">Your Goal Is Achieved</div>) : (null)}
+     
+      
+      
       </div>
 
       {/* {num === 3 ? () : ()} */}
@@ -90,11 +126,7 @@ export default function Heading() {
             </div>
           </div>
         </div>
-      ) : (
-        <div>
-        <div> No Data Found!</div>
-        </div>
-      )}
+      ) : null}
     </>
   );
 }
